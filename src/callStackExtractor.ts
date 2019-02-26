e1b59ef5fadeb796bb5d3597fa96331edc2fa562
Jay Mathew <hiojay@hotmail.com>
2019-02-26 22:12:31 +1000

    Fixed bug that was preventing some call constructor lines from showing in the extracted call stack

--- src/callStackExtractor.ts
+++ src/callStackExtractor.ts
@@ -80,7 +80,7 @@ module.exports = {
     let lines: RegExpMatchArray = fileContents.match(
       // /(?:(?:(?:start|end|resume|reend).*Nest=.*)|(?:call (?:int|private|method|getter|setter).*)|End-Function.*|end-get.*|end-set.*|end-method.*)/gm
       // Update: Looks like we no longer need to search for any enders. We can just use "return stack:" instead. This seems to be more accurate, especially in the case of constructors calling other methods.
-      /(?:(?:(?:start|end|resume|reend).*Nest=.*)|(?:call (?:int|private|method|getter|setter).*)|return stack:.*)/gm
+      /(?:(?:(?:start|end|resume|reend).*Nest=.*)|(?:call (?:int|private|method|getter|setter|constructor).*)|return stack:.*)/gm
     );
     // if there are lines in this session, store it in fileContents
     lines != null ? (fileContents = lines.join("\n")) : (fileContents = "");
@@ -185,15 +185,19 @@ module.exports = {
           // we haven't matched a start-ext, start, end-ext, end, resume or rend
           // look for calls to other methods/functions, or other enders
           // lineMatchResult = /(call (int|setter|getter|private|method)|End-Function|end-get|end-set|end-method|%s)/.exec(
-          lineMatchResult = /(call (int|setter|getter|private|method)|return stack:|%s)/.exec(
+          lineMatchResult = /(call (int|setter|getter|private|method|constructor)|return stack:|%s)/.exec(
             lineContents
           );
 
           // if we have found a match to calls to other methods/functions or other enders
           if (lineMatchResult && lineMatchResult.length > 0) {
             // if the last call was a start or a call then increment the nest level
-            if (lastCall == "start" || lastCall.slice(0, 4) == "call")
+            if (
+              lastCall == "start" ||
+              (lastCall.slice(0, 4) == "call" && lastCall != "callConstructor")
+            ) {
               nestLevel += 1;
+            }
 
             switch (lineMatchResult[1]) {
               case "call method":
@@ -245,7 +249,10 @@ module.exports = {
                   );
                 }
                 break;
-
+              case "call constructor":
+                lastCall = "callConstructor";
+                // Note: If it's a call constructor in this context then we know that this is standalone and no other nested calls will be made
+                break;
               case "return stack:":
                 lastCall = "endSomething";
                 nestLevel -= 1;
@@ -332,18 +339,51 @@ module.exports = {
       }
 
       // Add a tab to each line before resume until you reach an end for that event
-      resumeResults.forEach((resumeResultsLine : string, resumeResultsLineNo : number) => {
+      resumeResults.forEach(
+        (resumeResultsLine: string, resumeResultsLineNo: number) => {
+          // If there is actually an end within the same session, then format all lines after the end and before the resume
+          // to do: provide an example to help with debugging
+          // console.log(resumeResultsLineNo);
+          // console.log(resumeResultsLine);
+          // if we find the resumeResults line in end results it means we have an end within the same session??
+          if (this.findValue(endResults, resumeResultsLine) != null) {
+            // go backwards starting from resume results line number until you find an end
+            for (let x = resumeResultsLineNo; x >= 0; x--) {
+              let regexEndStatement = new RegExp(
+                `end\\s+${resumeResultsLine}`,
+                "g"
+              );
 
-        // If there is actually an end within the same session, then format all lines after the end and before the resume
-        // to do: provide an example to help with debugging
-        // console.log(resumeResultsLineNo);
-        // console.log(resumeResultsLine);
-        // if we find the resumeResults line in end results it means we have an end within the same session??
-        if (this.findValue(endResults, resumeResultsLine) != null) {
-          // go backwards starting from resume results line number until you find an end
-          for (let x = resumeResultsLineNo; x >= 0; x--) {
+              let matchEndStatementResult = regexEndStatement.exec(results[x]);
+
+              // if there is a match, we have reached the end statement, so we don't need to add any more tabs
+              if (
+                matchEndStatementResult &&
+                matchEndStatementResult.length > 0
+              ) {
+                // break out of the for loop
+                break;
+              } else {
+                if (x != resumeResultsLineNo) {
+                  results[x] = "\t" + results[x];
+                }
+              }
+            }
+          }
+        }
+      );
+      // End- Add a tab to each line before resume until you reach an end for that event
+
+      // Add a tab to each line before reend until you reach a resume for that event
+      reendResults.forEach(
+        (reendResultsLine: string, reendResultsLineNo: number) => {
+          // If there is actually an end within the same session, then format all lines after the end and before the resume
+          // to do: provide an example to help with debugging
+
+          // go backwards starting from reend results line number until you find a resume
+          for (let x = reendResultsLineNo; x >= 0; x--) {
             let regexEndStatement = new RegExp(
-              `end\\s+${resumeResultsLine}`,
+              `resume\\s.*?${reendResultsLine}`,
               "g"
             );
 
@@ -354,40 +394,13 @@ module.exports = {
               // break out of the for loop
               break;
             } else {
-              if (x != resumeResultsLineNo) {
+              if (x != reendResultsLineNo) {
                 results[x] = "\t" + results[x];
               }
             }
           }
         }
-      });
-      // End- Add a tab to each line before resume until you reach an end for that event
-
-      // Add a tab to each line before reend until you reach a resume for that event
-      reendResults.forEach((reendResultsLine : string, reendResultsLineNo : number) => {
-        // If there is actually an end within the same session, then format all lines after the end and before the resume
-        // to do: provide an example to help with debugging
-
-        // go backwards starting from reend results line number until you find a resume
-        for (let x = reendResultsLineNo; x >= 0; x--) {
-          let regexEndStatement = new RegExp(
-            `resume\\s.*?${reendResultsLine}`,
-            "g"
-          );
-
-          let matchEndStatementResult = regexEndStatement.exec(results[x]);
-
-          // if there is a match, we have reached the end statement, so we don't need to add any more tabs
-          if (matchEndStatementResult && matchEndStatementResult.length > 0) {
-            // break out of the for loop
-            break;
-          } else {
-            if (x != reendResultsLineNo) {
-              results[x] = "\t" + results[x];
-            }
-          }
-        }
-      }); // End - Add a tab to each line before reend until you reach an end for that event
+      ); // End - Add a tab to each line before reend until you reach an end for that event
 
       // store results in fileContents
       fileContents = results.join("\n");
@@ -409,13 +422,13 @@ module.exports = {
 
     // Prefix all remaining calls with call function
     fileContents = fileContents.replace(
-      /call\s(?!(?:method|getter|setter|%s))(.*(?!\.OnExecute))$/gm,
+      /call\s(?!(?:method|getter|setter|constructor|%s))(.*(?!\.OnExecute))$/gm,
       "call function $1"
     );
 
     // Rename eligible call methods to call constructors
     fileContents = fileContents.replace(
-      /call\smethod\s+(\w+)(\s.*\1.OnExecute)/gm,
+      /call\smethod\s+(\w+)(\s.*\1).OnExecute/gm,
       "call constructor $1$2"
     );
 
@@ -423,6 +436,14 @@ module.exports = {
     fileContents = fileContents.replace(
       /start-ext\s(\w+)\s(\w+\.\w+\.\w+)$/gm,
       "call function $1 $2"
+    );
+
+    // Replace any call constructors with the constructor name at the start
+    // e.g. call constructor SCC_FLUID:UTIL:CSSClass
+    // becomes call constructor CSSClass SCC_FLUID:UTIL:CSSClass
+    fileContents = fileContents.replace(
+      /call constructor\s(.*:)(\w+)/gm,
+      "call constructor $2 $1$2"
     );
 
     // Replace any remaining start-ext with calls based on the last word in the line
@@ -433,11 +454,15 @@ module.exports = {
       "call $3 $1 $2"
     );
 
-    // Finally rename CI functions
+    // Rename CI functions
     fileContents = fileContents.replace(
       /start-ext\s(\w+)\s(\w+\.\w+)$/gm,
       "call function $1 $2"
     );
+
+    // Remove .OnExecute
+    fileContents = fileContents.replace(/.OnExecute/gm, "");
+
     // Replace any colons with dots
     fileContents = fileContents.replace(/:/gm, ".");
 
@@ -462,19 +487,65 @@ module.exports = {
       let fileContents: string = fs.readFileSync(filePath, "UTF-8");
 
       let newString: string;
+      // regex: call constructor\s+(.*)\s+#params=
+      // Find all call constructors that have #params in them, since they will have a duplicate call constructor line that we must delete:
+      // For example:
+      // PSAPPSRV.4164 (411) 	 1-8101   14.25.53    0.000000                                 32:    &oReport = create SSR_TRANSCRIPT:Report();
+      // PSAPPSRV.4164 (411) 	 1-8102   14.25.53    0.000000                                     call constructor SSR_TRANSCRIPT:Report
+      // PSAPPSRV.4164 (411) 	 1-8103   14.25.53    0.000000                                     call constructor  SSR_TRANSCRIPT:Report #params=0
+      // PSAPPSRV.4164 (411) 	 1-8104   14.25.53    0.000000   >>> start-ext Nest=82 Report SSR_TRANSCRIPT.Report.OnExecute
+      // PSAPPSRV.4164 (411) 	 1-8105   14.25.53    0.000000                               >>>>> Begin SSR_TRANSCRIPT.Report.OnExecute level 0 row 0
+      let constructorLinestoRemove: string[] = [];
+      let regexp = /call constructor\s+(.*)\s+#params=/g;
+      let match = regexp.exec(fileContents);
+
+      while (match != null) {
+        constructorLinestoRemove.push(match[1]);
+        match = regexp.exec(fileContents);
+      }
+
+      constructorLinestoRemove = [...new Set(constructorLinestoRemove)]; // Make array unique
+
       // First remove all lines that have a call getter, call setter, call method or call constructor followed by a start-ext, since the start-ext is sufficient
       // For example: the following call method line will be ignored since there is a start-ext immediately after it:
       // PSAPPSRV.4556 (2426)   1-8760   14.05.07    0.000000       call method  SSF_CFS:SSF_CFQKey.SSFQKeyString #params=7
       // PSAPPSRV.4556 (2426)   1-8761   14.05.07    0.000000   >>> start-ext Nest=01 SSFQKeyString SSF_CFS.SSF_CFQKey.OnExecute getter
+      // Example 2: the following call constructor line will be ignored since there is a start-ext immediately after it:
+      // PSAPPSRV.146732 (3396) 	 1-3050250 15.11.25    0.000000                               call constructor  SAA_ACADEMIC_PROGRESS_FL:COMPONENTS:SAA_ACD_PRG_SM_FL #params=0
+      // PSAPPSRV.146732 (3396) 	 1-3050251 15.11.25    0.000000   >>> start-ext Nest=07 SAA_ACD_PRG_SM_FL SAA_ACADEMIC_PROGRESS_FL.COMPONENTS.SAA_ACD_PRG_SM_FL.OnExecute
       newString = fileContents.replace(
         /(?:.*call\s+(getter|setter|method|constructor).*)\r?\n(.*>>>\sstart-ext.*)/gm,
         "$2 $1"
       );
 
+      // Remove the superfluous call constructors that we identified earlier
+      constructorLinestoRemove.forEach(line => {
+        let regexp = new RegExp(
+          "(?:.*call\\s+constructor\\s+" + line + ")",
+          "gm"
+        );
+        newString = newString.replace(regexp, "");
+      });
+
       // Add an end-get for relevant lines
       // I.e. Find all internal call getters and add end-gets. This is done by matching line number before the call getter and after the getter method has finished
       // Note: This is really ugly, but I couldn't find a better way to do this. Plus it's all 'easily' done in one line of code
       // Note 2: This seems to make vscode hang for large files (e.g. 16mb)
+      // For example:
+      // PSAPPSRV.4164 (411) 	 1-10721  14.25.54    0.000000                               1002:    If %This.GetDatasource().Active = False Then
+      // PSAPPSRV.4164 (411) 	 1-10722  14.25.54    0.000000                                     call getter  PSXP_RPTDEFNMANAGER:DataSourceDefn.Active #params=2
+      // PSAPPSRV.4164 (411) 	 1-10723  14.25.54    0.000000   >>> start-ext Nest=84 Active PSXP_RPTDEFNMANAGER.DataSourceDefn.OnExecute
+      // PSAPPSRV.4164 (411) 	 1-10724  14.25.54    0.000000                               >>>>> Begin PSXP_RPTDEFNMANAGER.DataSourceDefn.OnExecute level 0 row 0
+      // PSAPPSRV.4164 (411) 	 1-10725  14.25.54    0.000000                                333: get Active
+      // PSAPPSRV.4164 (411) 	 1-10726  14.25.54    0.000000                                334:    If &DataSource.Active_flag = "A" Or
+      // PSAPPSRV.4164 (411) 	 1-10727  14.25.54    0.000000                                335:       Return ( True);
+      // PSAPPSRV.4164 (411) 	 1-10728  14.25.54    0.000000                                     return stack:
+      // PSAPPSRV.4164 (411) 	 1-10729  14.25.54    0.000000                                          UUUUUUUUUUUUUUUUUUU:DataSourceDefn
+      // PSAPPSRV.4164 (411) 	 1-10730  14.25.54    0.000000                                          Str[6]=Active
+      // PSAPPSRV.4164 (411) 	 1-10731  14.25.54    0.000000                                      return value:
+      // PSAPPSRV.4164 (411) 	 1-10732  14.25.54    0.000000                                          Bool=True
+      // PSAPPSRV.4164 (411) 	 1-10733  14.25.54    0.000000   <<< end-ext   Nest=84 Active PSXP_RPTDEFNMANAGER.DataSourceDefn.OnExecute Dur=0.000040 CPU=0.000000 Cycles=8
+      // PSAPPSRV.4164 (411) 	 1-10734  14.25.54    0.000000                               1002:    If %This.GetDatasource().Active = False Then
       newString = newString.replace(
         /(\d+:.*)(\r?\n.*call getter\s+(?:\w+:?)+\.\w+[\s\S]*?)(.*)(\1)/gm,
         "$1$2$3end-get;"
@@ -496,7 +567,9 @@ module.exports = {
         if (stats.sessionNos.length > 0) {
           /* This is a PSAPPSRV trace */
           /* sort sessionNos array */
-          stats.sessionNos.sort( function( a : number, b : number) { return a - b });
+          stats.sessionNos.sort(function(a: number, b: number) {
+            return a - b;
+          });
           stats.sessionNos.forEach((sessionNo: number) => {
             // Extract only those lines relating to the session number
             // console.log(`Extracting lines for sessionNo: ${sessionNo}`);
